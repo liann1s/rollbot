0426d12eb696c9667290247ddd121af32889d597
liann1s <liannis@gmail.com>
2025-04-27 15:32:16 -0500

    Update deploy-commands.js
    switch to global commands

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,10 +1,16 @@
-require('dotenv').config();
 const { REST, Routes, SlashCommandBuilder } = require('discord.js');
+require('dotenv').config();
 
 const commands = [
-  new SlashCommandBuilder().setName('roll').setDescription('Roll the productivity dice!'),
-  new SlashCommandBuilder().setName('stats').setDescription('View your productivity stats.'),
-  new SlashCommandBuilder().setName('history').setDescription('View your recent rolls history.')
+  new SlashCommandBuilder()
+    .setName('roll')
+    .setDescription('Roll a d20 to determine your task fate!'),
+  new SlashCommandBuilder()
+    .setName('stats')
+    .setDescription('View your productivity stats.'),
+  new SlashCommandBuilder()
+    .setName('history')
+    .setDescription('View your recent roll history.')
 ]
 .map(command => command.toJSON());
 
@@ -12,14 +18,14 @@ const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 
 (async () => {
   try {
-    console.log('Started refreshing application (/) commands.');
+    console.log('Started refreshing GLOBAL application (/) commands.');
 
     await rest.put(
-      Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+      Routes.applicationCommands(process.env.CLIENT_ID),
       { body: commands },
     );
 
-    console.log('Successfully reloaded application (/) commands.');
+    console.log('Successfully reloaded GLOBAL application (/) commands.');
   } catch (error) {
     console.error(error);
   }
